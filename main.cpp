0b2f0e1c76bbf5c399440a92a2f69c49dd42a94f
maciej88 <mkrywult88@gmail.com>
2022-10-21 22:03:37 +0200

    drawning character

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@ int main()
 {
     // window dimensions
     int windowDimensions[2];
-    windowDimensions[0] = 384;
-    windowDimensions[1] = 384;
+    windowDimensions[0] = 384; // width
+    windowDimensions[1] = 384; // height
     // initialize window
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
 
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
     Vector2 mapPos{0.0, 0.0};
     float speed{4.0};
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        (float)windowDimensions[0]/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
+        (float)windowDimensions[1]/2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
 
 
     SetTargetFPS(60);
@@ -32,7 +38,13 @@ int main()
             
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
         }
+        // draw map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // draw the character
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
         // stop drawning
         EndDrawing();
