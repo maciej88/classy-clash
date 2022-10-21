dcb65384150ca8fa95c75c5f1238faaa2aa3be83
maciej88 <mkrywult88@gmail.com>
2022-10-21 18:55:00 +0200

    refactoring

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
     // initialize window
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
 
+    Texture2D background = LoadTexture("nature_tileset/WorldMap.png");
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
