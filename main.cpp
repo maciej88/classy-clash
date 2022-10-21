8d7979b7be893722d7a6705a4fdf43cca2b792f6
maciej88 <mkrywult88@gmail.com>
2022-10-21 21:01:29 +0200

    movement add

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -10,6 +11,9 @@ int main()
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
 
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
+
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -17,7 +21,17 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mapPos{0.0, 0.0};
+         Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         // stop drawning
