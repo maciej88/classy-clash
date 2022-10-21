f1ea885b39c520b62228b95455d0b660be66dbf4
maciej88 <mkrywult88@gmail.com>
2022-10-21 19:25:43 +0200

    World map as bg

--- main.cpp
+++ main.cpp
@@ -9,12 +9,16 @@ int main()
     // initialize window
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
 
-    Texture2D background = LoadTexture("nature_tileset/WorldMap.png");
+    Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        BeginDrawing();
+        ClearBackground(WHITE);
 
+        Vector2 mapPos{0.0, 0.0};
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         // stop drawning
         EndDrawing();
