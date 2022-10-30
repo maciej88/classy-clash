086f178720f55674d0cf1da2a329c4f325a0d74f
maciej88 <mkrywult88@gmail.com>
2022-10-30 16:48:03 +0100

    prop rozk add on map

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
+#include "Prop.h"
 
 int main()
 {
@@ -17,6 +18,8 @@ int main()
 
     Character knight{windowWidth, windowHeight};
 
+     Prop rock{Vector2{0.f, 0.f}, LoadTexture("nature_tileset/Rock.png")};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -27,6 +30,8 @@ int main()
 
         // draw map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+
+        rock.Render(knight.getWorldPos());
 
         knight.tick(GetFrameTime());
 
