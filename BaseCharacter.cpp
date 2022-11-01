438c4e2cd62c8da91cee2bb83464b9f6ee56a1e4
maciej88 <mkrywult88@gmail.com>
2022-11-01 14:16:13 +0100

    code review

--- BaseCharacter.cpp
+++ BaseCharacter.cpp
@@ -1,6 +1,22 @@
 #include "BaseCharacter.h"
+#include "raylib.h"
 
 BaseCharacter::BaseCharacter()
 {
-    
+}
+
+void BaseCharacter::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle BaseCharacter::getCollisionRec()
+{
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale
+    };
+   
 }
--- Enemy.cpp
+++ Enemy.cpp
@@ -1,11 +1,11 @@
 #include "Enemy.h"
 
-Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) :
-    worldPos(pos),
-    texture(idle_texture),
-    idle(idle_texture),
-    run(run_texture)
+Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture):
 {
+     worldPos = pos;
+    texture = idle_texture;
+    idle = idle_texture;
+    run = run_texture;
     width = texture.width / maxFrames;
     height = texture.height;
 }
@@ -30,17 +30,4 @@ void Enemy::tick(float deltaTime)
     DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
 }
 
-void Enemy::undoMovement()
-{
-    worldPos = worldPosLastFrame;
-}
 
-Rectangle Enemy::getCollisionRec()
-{
-    return Rectangle{
-        screenPos.x,
-        screenPos.y,
-        width * scale,
-        height * scale
-    };
-}
