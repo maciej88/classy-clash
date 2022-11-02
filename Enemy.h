cb388ebbdba8b8dfc27f32cca728790ddc8b9208
maciej88 <mkrywult88@gmail.com>
2022-11-02 18:01:35 +0100

    variable adressing

--- Enemy.h
+++ Enemy.h
@@ -1,10 +1,13 @@
 #include "raylib.h"
 #include "Character.h"
+#include "BaseCharacter.h"
 
 class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
     virtual void tick(float deltaTime) override;
+    void setTarget(Character* character) { target = character; }
 private:
+    Character* target;
 };
